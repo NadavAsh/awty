efd25f285bdb9f9912e69370a6ee4dbf6538d389
Nadav Ashkenazi <ashkenazi.nadav@gmail.com>
2015-05-11 02:13:30 -0700

    Added buttons, edit text, and alarm broadcast intent and receiver.

--- app/src/main/java/edu/washington/nadava/awty/MainActivity.java
+++ app/src/main/java/edu/washington/nadava/awty/MainActivity.java
@@ -1,17 +1,69 @@
 package edu.washington.nadava.awty;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+    public static final String MESSAGE = "com.washington.nadava.awty.sendmessage.MESSAGE";
+    private boolean running;
+    PendingIntent alarmIntent;
+    BroadcastReceiver alarmReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alarmReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                Toast.makeText(MainActivity.this, intent.getStringExtra(MESSAGE), Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        registerReceiver(alarmReceiver, new IntentFilter("com.washington.nadava.awty.sendmessage"));
+
+        final AlarmManager alarmManager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+
+        final TextView messageText = (TextView)findViewById(R.id.editText);
+        final Button toggle = (Button)findViewById(R.id.toggleButton);
+        toggle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                running = !running;
+
+
+                if (running) {
+                    toggle.setText(R.string.stop);
+
+                    Intent i = new Intent();
+                    i.setAction("com.washington.nadava.awty.sendmessage");
+                    i.putExtra(MESSAGE, messageText.getText().toString());
+                    alarmIntent = PendingIntent.getBroadcast(MainActivity.this, 0, i,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+                    alarmManager.setRepeating(AlarmManager.RTC, System.currentTimeMillis() + 10000,
+                                              10000, alarmIntent);
+                } else {
+                    toggle.setText(R.string.start);
+                    alarmIntent.cancel();
+                }
+
+            }
+        });
     }
 
     @Override
